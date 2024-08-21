e1b38417437e5010d6477aebb3ade25dc182f332
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-21 15:42:53 +0530

    Add files via upload

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,47 +1,70 @@
-import { Text, View } from "react-native"
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { SafeAreaView } from "react-native"
 import { router } from "expo-router";
-import Swiper from "react-native-swiper";
 import { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constants";
 
-const Welcome = () => {
-    const swiperRef = useRef<Swiper>(null);
-    const [activeIndex, setActiveIndex] = useState(0);
-    return (
-        <SafeAreaView className="flex h-full items-center justify-between bg-white">
-          <TouchableOpacity
-            onPress={() => {
-              router.replace("/(auth)/sign-up");
-            }}
-            className="w-full flex justify-end items-end p-5"
-          >
-            <Text className="text-black text-md font-JakartaBold">Skip</Text>
-          </TouchableOpacity>
+const Home = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-          <Swiper
-            ref={swiperRef}
-            loop={false}
-            dot={
-                <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
-            }
-            activeDot={
-                <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
-            }
-            onIndexChanged={(index) => setActiveIndex(index)}
-          >
-            {onboarding.map((item) => (
-                <View 
-                    className="flex items-center justify-center p-5"
-                    key={item.id}
-                >
-                    <Text>{item.title}</Text>
-                </View>
-            ))}
-          </Swiper>
-        </SafeAreaView>
-    )
-}
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
-export default Welcome;
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-md font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10 mb-5"
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
--- constants/index.ts
+++ constants/index.ts
@@ -76,21 +76,21 @@ export const onboarding = [
     id: 1,
     title: "The perfect ride is just a tap away!",
     description:
-      "Your journey begins with Ryde. Find your ideal ride effortlessly.",
+      "Start your journey with Urban Wheels. Find your ideal ride with ease.",
     image: images.onboarding1,
   },
   {
     id: 2,
-    title: "Best car in your hands with Ryde",
+    title: "Urban Wheels: Your car, your choice",
     description:
-      "Discover the convenience of finding your perfect ride with Ryde",
+      "Experience the simplicity of finding your perfect ride with Urban Wheels.",
     image: images.onboarding2,
   },
   {
     id: 3,
     title: "Your ride, your way. Let's go!",
     description:
-      "Enter your destination, sit back, and let us take care of the rest.",
+      "Enter your destination, relax, and let Urban Wheels handle the rest.",
     image: images.onboarding3,
   },
 ];
