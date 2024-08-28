afef9a3c3434e47ec146d78f4af3d30d78ee77d6
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-28 14:42:01 +0530

    Create Payment.tsx

---
+++ components/Payment.tsx
@@ -0,0 +1,34 @@
+import { useAuth } from "@clerk/clerk-expo";
+// import { useStripe } from "@stripe/stripe-react-native";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Image, Text, View } from "react-native";
+import { ReactNativeModal } from "react-native-modal";
+
+import CustomButton from "@/components/CustomButton";
+import { images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
+import { useLocationStore } from "@/store";
+import { PaymentProps } from "@/types/type";
+
+const Payment = ({
+    fullName,
+    email,
+    amount,
+    driverId,
+    rideTime,
+}: PaymentProps) => {
+    // const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    const {
+        userAddress,
+        userLongitude,
+        userLatitude,
+        destinationLatitude,
+        destinationAddress,
+        destinationLongitude,
+    } = useLocationStore();
+    const { userId } = useAuth();
+    const [success, setSuccess] = useState<boolean>(false);
+}
+
+export default Payment;
