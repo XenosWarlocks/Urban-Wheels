a7b37c8fdcf1852d51674e7f1da4fc99261c92ad
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-28 14:19:30 +0530

    Update book-ride.tsx

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -1,11 +1,29 @@
+import { useUser } from "@clerk/clerk-expo";
+// import { StripeProvider } from "@stripe/stripe-react-native";
 import { Image, Text, View } from "react-native";
 
+// import Payment from "@/components/Payment";
+import RideLayout from "@/components/RideLayout";
+import { icons } from "@/constants";
+import { formatTime } from "@/lib/utils";
+import { useDriverStore, useLocationStore } from "@/store";
 
 const BookRide = () => {
+    const { user } = useUser();
+    const { userAddress, destinationAddress } = useLocationStore();
+    const { drivers, selectedDriver } = useDriverStore();
+
+    const driverDetails = drivers?.filter(
+        (driver) => driver.id === selectedDriver,   
+    )[0];
     return (
-        <View>
-            <View>Book Ride</View>
-        </View>
+        <RideLayout>
+            <>
+                <Text className="text-xl font-JakartaSemiBold mb-3">
+                    Ride Information
+                </Text>
+            </>
+        </RideLayout>
     );
 }
 
