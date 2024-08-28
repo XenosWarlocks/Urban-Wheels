85538a893baed68703d8e53a22fee6ba32352b2f
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-28 14:55:17 +0530

    Update Payment.tsx

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -74,10 +74,41 @@ const Payment = ({
                             headers: {
                                 "Content-Type": "application/json",
                             },
-                        })
+                            body: JSON.stringify({
+                                payment_method_id: paymentMethod.id,
+                                payment_intent_id: paymentIntent.id,
+                                customer_id: customer,
+                                client_secret: paymentIntent.client_secret,
+                            }),
+                        });
+                        if (result.client_secret) {
+                            await fetchAPI("/(api)/ride/create", {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                                body: JSON.stringify({
+                                    origin_address: userAddress,
+                                    destination_address: destinationAddress,
+                                    origin_latitude: userLatitude,
+                                    origin_longitude: userLongitude,
+                                    destination_latitude: destinationLatitude,
+                                    destination_longitude: destinationLongitude,
+                                    ride_time: rideTime.toFixed(0),
+                                    fare_price: parseInt(amount) * 100,
+                                    payment_status: "paid",
+                                    driver_id: driverId,
+                                    user_id: userId,
+                                }),
+                            });
+                            intentCreationCallback({
+                                clientSecret: result.client_secret,
+                            });
+                        }
                     }
-                }
-            }
+                },
+            },
+            returnURL: "myapp://book-ride", 
         })
     }
 }
