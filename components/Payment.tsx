f15815937dfa6d5e69c1800fca0fca4ff6c799a9
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-28 15:03:22 +0530

    Update Payment.tsx

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -110,7 +110,39 @@ const Payment = ({
             },
             returnURL: "myapp://book-ride", 
         })
-    }
+        if (error) {
+            // setLoading(true);
+        }
+    };
+    return (
+        <>
+            <CustomButton
+                title="Confirm Ride"
+                className="my-10"
+                onPress={openPaymentSheet}
+            />
+            <ReactNativeModal
+                isVisible={success}
+                onBackdropPress={() => setSuccess(false)}
+            >
+                <View className="flex flex-col items-center justify-center bg-white p-7 rounded-2xl">
+                    <Image source={images.check} className="w-28 h-28 mt-5"/>
+                    <Text className="text-2xl text-center font-JakartaBold mt-5">Booking placed successfully</Text>
+                    <Text className="text-md text-general-200 font-JakartaRegular text-center mt-3">
+                    Thank you for your booking. Your reservation has been successfully placed. Please proceed with your trip.
+                    </Text>
+                    <CustomButton
+                        title="Back Home"
+                        onPress={() => {
+                            setSuccess(false);
+                            router.push("/(root)/(tabs)/home")
+                        }}
+                        className="mt-5"
+                    />
+                </View>
+            </ReactNativeModal>
+        </>
+    )
 }
 
 export default Payment;
