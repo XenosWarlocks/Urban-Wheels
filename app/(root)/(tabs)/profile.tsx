3bd95d8573c142bd6324fb6f5bc16409826ce63f
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-30 09:21:36 +0530

    Update profile.tsx

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,5 +1,8 @@
-import { Text, View } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import InputField from "@/components/inputField";
 
 const Profile = () => {
     return (
