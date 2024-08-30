f3e0005b3f502ebdb2566398d75d6cfce31f54db
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-30 09:30:37 +0530

    Update chat.tsx

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "@/constants";
 
 const Chat = () => {
     return (
-        <SafeAreaView>
-            <Text>Chat</Text>
+        <SafeAreaView className="flex-1 bg-white p-5">
+            <ScrollView>
+                <Text></Text>
+                <View>
+                    <Image/>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
