821c4ad05d47ba3bda74176b73a3abdb9ce35b30
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-30 09:32:57 +0530

    Update chat.tsx

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -6,10 +6,21 @@ import { images } from "@/constants";
 const Chat = () => {
     return (
         <SafeAreaView className="flex-1 bg-white p-5">
-            <ScrollView>
-                <Text></Text>
-                <View>
-                    <Image/>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                <Text className="text-2xl font-JakartaBold">Chat</Text>
+                <View className="flex-1 h-fit flex justify-center items-center">
+                    <Image
+                        source={images.message}
+                        alt="message"
+                        className="w-full h-40"
+                        resizeMode="contain"
+                    />
+                    <Text className="text-3xl font-JakartaBold mt-3">
+                        No Messages Yet
+                    </Text>
+                    <Text className="text-base mt-2 text-center px-7">
+                        Start a conversation
+                    </Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
