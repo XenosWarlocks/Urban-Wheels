0afec2c2f8ecbda3cc15f18866040b5a11bd5d07
XenosWarlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-08-30 09:11:38 +0530

    Update rides.tsx

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -1,5 +1,11 @@
-import { Text, View } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import RideCard from "@/components/RideCard";
+import { images } from "@/constants";
+import { useFetch } from "@/lib/fetch";
+import { Ride } from "@/types/type";
 
 const Rides = () => {
     return (
